297e402fa284c2eb97d61688450c244edbceb56b
Dyuman Aditya <dyuman2001@gmail.com>
2025-08-15 17:52:48 +0200

    to be evaled

--- dflex/dflex/envs/go2_velocity.py
+++ dflex/dflex/envs/go2_velocity.py
@@ -94,17 +94,17 @@ class Go2VelocityEnv(DFlexEnv):
         self._actions = torch.zeros(self.num_envs, num_act, device=self.device)
         
         # Rewards: Following IsaacLab reward structure
-        self.lin_vel_reward_scale = 5.0
-        # self.lin_vel_reward_scale = 1.0
-        self.yaw_rate_reward_scale = 2.0
-        # self.yaw_rate_reward_scale = 0.5
-        self.z_vel_reward_scale = -5.0
+        # self.lin_vel_reward_scale = 5.0
+        self.lin_vel_reward_scale = 1.0
+        # self.yaw_rate_reward_scale = 2.0
+        self.yaw_rate_reward_scale = 0.5
+        self.z_vel_reward_scale = -2.0
         self.ang_vel_reward_scale = -0.05
         self.joint_torque_reward_scale = -2.5e-5
         self.joint_accel_reward_scale = -2.5e-7
         self.action_rate_reward_scale = -0.01
         self.feet_air_time_reward_scale = 0.25
-        self.undesired_contact_reward_scale = -2.0
+        self.undesired_contact_reward_scale = -3.0
         self.flat_orientation_reward_scale = -5.0
 
         # Early termination parameters
