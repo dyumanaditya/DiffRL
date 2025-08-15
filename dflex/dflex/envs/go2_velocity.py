e4dc009db65895197357bcc66a43003d7adbaa08
Dyuman Aditya <dyuman2001@gmail.com>
2025-08-15 21:38:27 +0200

    mod contact params

--- dflex/dflex/envs/go2_velocity.py
+++ dflex/dflex/envs/go2_velocity.py
@@ -105,14 +105,14 @@ class Go2VelocityEnv(DFlexEnv):
         # self.lin_vel_reward_scale = 1.0
         self.yaw_rate_reward_scale = 5.0
         # self.yaw_rate_reward_scale = 0.5
-        self.z_vel_reward_scale = -2.0
-        self.ang_vel_reward_scale = -0.05
+        self.z_vel_reward_scale = -3.0
+        self.ang_vel_reward_scale = -0.15
         self.joint_torque_reward_scale = -2.5e-5
         self.joint_accel_reward_scale = -2.5e-7
         self.action_rate_reward_scale = -0.01
-        self.feet_air_time_reward_scale = 0.25
+        self.feet_air_time_reward_scale = 0.15
         self.undesired_contact_reward_scale = -8.0
-        self.flat_orientation_reward_scale = -10.0
+        self.flat_orientation_reward_scale = -8.0
 
         # Early termination parameters
         self.termination_height = termination_height
@@ -190,7 +190,7 @@ class Go2VelocityEnv(DFlexEnv):
         """Sample new velocity commands for specified environments"""
         # Sample linear velocity commands (x, z) and angular velocity (y)
         # Following IsaacLab pattern: uniform distribution between -1 and 1
-        self._commands[env_ids] = torch.zeros_like(self._commands[env_ids]).uniform_(-1.0, 1.0)
+        self._commands[env_ids] = torch.zeros_like(self._commands[env_ids]).uniform_(-2.0, 2.0)
         # self._commands[env_ids] = torch.zeros_like(self._commands[env_ids]) + torch.tensor(
         #     [1.0, 0.0, 0.0], device=self.device, dtype=torch.float32
         # )
