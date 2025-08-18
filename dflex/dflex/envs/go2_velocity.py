0dde2fd37f623da7cd52edd5ec8490908cfd4df2
Dyuman Aditya <dyuman2001@gmail.com>
2025-08-18 15:42:14 +0200

    to be evaled

--- dflex/dflex/envs/go2_velocity.py
+++ dflex/dflex/envs/go2_velocity.py
@@ -93,7 +93,7 @@ class Go2VelocityEnv(DFlexEnv):
         self.init_sim()
 
         # MDP parameters
-        self.action_scale = 1.0
+        self.action_scale = 0.5
 
         # Velocity tracking parameters
         self._commands = torch.zeros(self.num_envs, 3, device=self.device)  # [lin_vel_x, lin_vel_y, ang_vel_z]
@@ -772,7 +772,7 @@ class Go2VelocityEnv(DFlexEnv):
             # "dof_torques_l2": joint_torques * self.joint_torque_reward_scale * self.sim_dt,
             # "dof_acc_l2": joint_accel * self.joint_accel_reward_scale * self.sim_dt,
             # "action_rate_l2": action_rate * self.action_rate_reward_scale * self.sim_dt,
-            # "feet_air_time": air_time * self.feet_air_time_reward_scale * self.sim_dt,
+            "feet_air_time": air_time * self.feet_air_time_reward_scale * self.sim_dt,
             "undesired_contacts": contacts * self.undesired_contact_reward_scale * self.sim_dt,
             "flat_orientation_l2": flat_orientation * self.flat_orientation_reward_scale * self.sim_dt,
         }
