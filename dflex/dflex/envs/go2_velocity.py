fa7ff53cfc758c9bb68cb3d06a46ec9b700c48e8
Dyuman Aditya <dyuman2001@gmail.com>
2025-08-19 21:28:38 +0200

    to be evaled

--- dflex/dflex/envs/go2_velocity.py
+++ dflex/dflex/envs/go2_velocity.py
@@ -109,18 +109,18 @@ class Go2VelocityEnv(DFlexEnv):
         self.torso_height = torch.zeros(self.num_envs, device=self.device) + 0.35  # Initial height
         
         # Rewards: Following IsaacLab reward structure
-        self.lin_vel_reward_scale = 5.0
+        self.lin_vel_reward_scale = 2.0
         # self.lin_vel_reward_scale = 1.0
-        self.yaw_rate_reward_scale = 3.0
+        self.yaw_rate_reward_scale = 1.0
         self.yaw_rate_reward_scale = 0.5
-        self.z_vel_reward_scale = -4.0
+        self.z_vel_reward_scale = -2.0
         self.ang_vel_reward_scale = -0.05
         self.joint_torque_reward_scale = -1e-5
         self.joint_accel_reward_scale = -2.5e-7
         self.action_rate_reward_scale = -0.01
         self.feet_air_time_reward_scale = 0.25
-        self.undesired_contact_reward_scale = -5.0
-        self.flat_orientation_reward_scale = -5.5
+        self.undesired_contact_reward_scale = -2.0
+        self.flat_orientation_reward_scale = -2.5
 
         # Early termination parameters
         self.termination_height = termination_height
@@ -437,7 +437,7 @@ class Go2VelocityEnv(DFlexEnv):
                     "termination/nonfinite_obs": nonfinite_mask.sum().item(),
                     "termination/invalid_joint_q": (torch.abs(joint_q) > 1e6).sum().item(),
                     "termination/invalid_joint_qd": (torch.abs(joint_qd) > 1e6).sum().item(),
-                    "termination/height": (obs[:, 48] < self.termination_height).sum().item() if self.early_termination else 0,
+                    "termination/height": (self.torso_height < self.termination_height).sum().item() if self.early_termination else 0,
                     "termination/total": termination.sum().item()
                 }
                 wandb.log(termination_reasons, step=self._current_step)
